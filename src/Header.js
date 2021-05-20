351564840a8fa3cd21a3006b1b00b9fdd9ac0358
vindard <17693119+vindard@users.noreply.github.com>
2021-05-19 21:32:40 -0400

    add fb icon and search field to header left

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Header.css"
+import SearchIcon from "@material-ui/icons/Search"
 
 function Header() {
     return (
         <div className="header">
-            <div className="header__left"></div>
+            <div className="header__left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
             <div className="header__middle"></div>
             <div className="header__right"></div>
         </div>
