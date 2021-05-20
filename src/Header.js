cb968b3869638aedec6fbb1a60f27f252f64fb24
vindard <17693119+vindard@users.noreply.github.com>
2021-05-19 21:37:19 -0400

    add header middle icons

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import "./Header.css"
 import SearchIcon from "@material-ui/icons/Search"
+import HomeIcon from '@material-ui/icons/Home'
+import FlagIcon from '@material-ui/icons/Flag'
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+
 
 function Header() {
     return (
@@ -12,7 +18,25 @@ function Header() {
                     <input type="text" />
                 </div>
             </div>
-            <div className="header__middle"></div>
+
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+            </div>
+
             <div className="header__right"></div>
         </div>
     )
