9f47096d2c7b40a30db940b74514b8d209adda92
vindard <17693119+vindard@users.noreply.github.com>
2021-05-19 21:44:05 -0400

    add header right clickable icons

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,14 @@ import "./Header.css"
 import SearchIcon from "@material-ui/icons/Search"
 import HomeIcon from '@material-ui/icons/Home'
 import FlagIcon from '@material-ui/icons/Flag'
-import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
-import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
-import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
-
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined'
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined'
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
+import { Avatar, IconButton } from "@material-ui/core"
+import AddIcon from '@material-ui/icons/Add'
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 function Header() {
     return (
@@ -37,7 +41,24 @@ function Header() {
                 </div>
             </div>
 
-            <div className="header__right"></div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>vindard</h4>
+                </div>
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+            </div>
         </div>
     )
 }
