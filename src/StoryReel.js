060e93c459f2fd43340166d36c86875acb723cf4
vindard <17693119+vindard@users.noreply.github.com>
2021-05-22 23:27:02 -0400

    add one more story card, Johns :p

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -7,23 +7,28 @@ function StoryReel() {
         <div className="storyReel">
             <Story
                 image="https://images.squarespace-cdn.com/content/v1/5e2372d18e3cf11ebaf2d20d/1600023546678-9Y4XFZKE4SW2EC01ID9Z/ke17ZwdGBToddI8pDm48kKaw-yiWCUT_dIkH-tML1b17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmgbBA9KfxWpJIziZzus6eiKk2902cuAFAxZ2IsJyg1YVeZUliRyJ_WuC65h3JNMsj/Cover_Vol1_%281000x1333_150dpi%29.jpg"
-                profileSrc="https://avatars.githubusercontent.com/u/288011?v=4"
+                profileSrc="https://avatars.githubusercontent.com/u/288011"
                 title="Jameson Lopp"
             />
             <Story
                 image="https://images.squarespace-cdn.com/content/v1/5e2372d18e3cf11ebaf2d20d/1600023613462-3UTX4GMDJ7SOCESSCKS5/ke17ZwdGBToddI8pDm48kKaw-yiWCUT_dIkH-tML1b17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmgbBA9KfxWpJIziZzus6eiKk2902cuAFAxZ2IsJyg1YVeZUliRyJ_WuC65h3JNMsj/Cover_Vol2_update_%281000x1333_150dpi%29.jpg"
-                profileSrc="https://avatars.githubusercontent.com/u/36541669?v=4"
+                profileSrc="https://avatars.githubusercontent.com/u/36541669"
                 title="Raj"
             />
             <Story
                 image="https://images.squarespace-cdn.com/content/v1/5e2372d18e3cf11ebaf2d20d/1600023628836-KVI6UD1WTUFLTMX5UTLX/ke17ZwdGBToddI8pDm48kKaw-yiWCUT_dIkH-tML1b17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmgbBA9KfxWpJIziZzus6eiKk2902cuAFAxZ2IsJyg1YVeZUliRyJ_WuC65h3JNMsj/Cover_Vol3_%281000x1333_150dpi%29_v3.jpg"
-                profileSrc="https://avatars.githubusercontent.com/u/30005204?v=4"
+                profileSrc="https://avatars.githubusercontent.com/u/30005204"
                 title="Billy Garrison"
             />
             <Story
                 image="https://images.squarespace-cdn.com/content/v1/5e2372d18e3cf11ebaf2d20d/1611238107339-PG3WQCWUKY0RIVEN3B1A/ke17ZwdGBToddI8pDm48kKaw-yiWCUT_dIkH-tML1b17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmgbBA9KfxWpJIziZzus6eiKk2902cuAFAxZ2IsJyg1YVeZUliRyJ_WuC65h3JNMsj/Cover_Vol9_%281000x1333_150dpi%29.jpg"
-                profileSrc="https://avatars.githubusercontent.com/u/971941?v=4"
+                profileSrc="https://avatars.githubusercontent.com/u/971941"
                 title="Fodé Diop"
+            />
+            <Story
+                image="https://images.squarespace-cdn.com/content/v1/5e2372d18e3cf11ebaf2d20d/bcf9009f-9076-432e-903c-d0915b5b7d16/ke17ZwdGBToddI8pDm48kKaw-yiWCUT_dIkH-tML1b17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmgbBA9KfxWpJIziZzus6eiKk2902cuAFAxZ2IsJyg1YVeZUliRyJ_WuC65h3JNMsj/Cover_Vol11_%281000x1333_150dpi%29.jpg"
+                profileSrc="https://avatars.githubusercontent.com/u/183140"
+                title="Johns Beharry"
             />
         </div>
     )
