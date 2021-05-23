2ad4f5a523a8db5caebec3e11b16f436702b9db0
vindard <17693119+vindard@users.noreply.github.com>
2021-05-22 21:55:37 -0400

    style header middle div

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Header() {
             </div>
 
             <div className="header__middle">
-                <div className="header__option">
+                <div className="header__option header__option--active">
                     <HomeIcon fontSize="large" />
                 </div>
                 <div className="header__option">
