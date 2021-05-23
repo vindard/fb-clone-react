3ad8d87898c1e8d993bb2801ee9a5f6c4c8aa225
vindard <17693119+vindard@users.noreply.github.com>
2021-05-22 23:37:30 -0400

    style app/sidebar/feed to position story cards

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="app">
       <Header />
 
-      <div className="app_body">
+      <div className="app__body">
         <Sidebar user={
           {
             photoURL: "https://avatars.githubusercontent.com/u/17693119?v=4",
