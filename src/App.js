6ce1701eaefb39b8a99b7072ec54deba07170b13
vindard <17693119+vindard@users.noreply.github.com>
2021-05-27 13:23:31 -0400

    build & style Login component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widget from './Widget';
+import Login from './Login';
 
 function App() {
+  const user = null
+
   return (
     // BEM  naming convention
     <div className="app">
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-      <div className="app__body">
-        <Sidebar user={
-          {
-            photoURL: "https://avatars.githubusercontent.com/u/17693119?v=4",
-            displayName: "Arvin R"
-          }
-        }/>
-        <Feed />
-        <Widget />
-      </div>
+          <div className="app__body">
+            <Sidebar user={
+              {
+                photoURL: "https://avatars.githubusercontent.com/u/17693119?v=4",
+                displayName: "Arvin R"
+              }
+            }/>
+            <Feed />
+            <Widget />
+          </div>
+        </>
+      )}
 
     </div>
   );
