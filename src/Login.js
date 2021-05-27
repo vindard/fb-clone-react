e936c676f896f50b8a080d853f1449d1d99c50ab
vindard <17693119+vindard@users.noreply.github.com>
2021-05-27 13:58:06 -0400

    implement signIn function on Login button click to get user

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,16 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 import "./Login.css"
+import { auth, provider } from "./firebase"
 
 function Login() {
     const signIn = () => {
-        // sign in ...
+        auth
+            .signInWithPopup(provider)
+            .then( (result) => {
+                console.log(result)
+            })
+            .catch( (error) => alert(error.message) )
     }
 
     return (
@@ -13,7 +19,7 @@ function Login() {
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
                 <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt="" />
             </div>
-            <Button type="submit" onCLick={signIn}>Sign In</Button>
+            <Button type="submit" onClick={signIn}>Sign In</Button>
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import firebaseConfig from  "./firebaseConfig"
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore()
 const auth = firebase.auth()
-const provider = firebase.auth.GoogleAuthProvider()
+const provider = new firebase.auth.GoogleAuthProvider()
 
 export { auth, provider }
 export default db
